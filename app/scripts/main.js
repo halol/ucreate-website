4ee42ca574fd0a22fd6e7acf3a52c072077099e4
Grzegorz Hadala <grzegorz.hadala@gmail.com>
2017-03-28 17:51:35 +0100

    Well

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+$(document).ready(function() {
+  $('.scroll-to').click(function(e) {
+    e.preventDefault();
+    var end = $(this).attr('data-end');
+    $('html, body').animate({
+      scrollTop: $(end).offset().top
+    }, 2000);
+  });
+});
